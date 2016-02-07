7c6504b72e9aae5b5b01fa0edf6dce72aa8f378c
Raphael Michel <mail@raphaelmichel.de>
2016-02-07 11:41:08 +0100

    Made field Preorder.warning_text optional

--- src/c6sh/core/models/preorder.py
+++ src/c6sh/core/models/preorder.py
@@ -4,7 +4,7 @@ from django.db import models
 class Preorder(models.Model):
     order_code = models.CharField(max_length=254, db_index=True)
     is_paid = models.BooleanField(default=False)
-    warning_text = models.TextField()
+    warning_text = models.TextField(blank=True)
 
     def __str__(self):
         return self.order_code
