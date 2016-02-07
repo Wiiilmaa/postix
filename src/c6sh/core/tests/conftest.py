a2baf369605e415aad579b14472084aea4ee9cc5
rixx <rixx@cutebit.de>
2016-02-07 11:42:29 +0100

    more tests

--- src/c6sh/core/tests/conftest.py
+++ src/c6sh/core/tests/conftest.py
@@ -1,5 +1,6 @@
 import datetime
 import random
+import string
 
 from faker import Factory
 import pytest
@@ -9,13 +10,13 @@ import pytest
 def user():
     from c6sh.core.models import User
     fake = Factory.create('en-US')
-    return User(username=fake.user_name(),
-                password=fake.password(),
-                firstname=fake.first_name(),
-                lastname=fake.last_name(),
-                is_active=True,
-                is_superuser=False,
-                is_troubleshooter=False)
+    return User.objects.create(username=fake.user_name(),
+                               password=fake.password(),
+                               firstname=fake.first_name(),
+                               lastname=fake.last_name(),
+                               is_active=True,
+                               is_superuser=False,
+                               is_troubleshooter=False)
 
 
 @pytest.fixture
@@ -34,40 +35,43 @@ def superuser(user):
 def item():
     from c6sh.core.models import Item
     fake = Factory.create('en-US')
-    return Item(name=fake.state(),
-                description=fake.bs(),
-                initial_stock=random.randint(50, 1000))
+    return Item.objects.create(name=fake.state(),
+                               description=fake.bs(),
+                               initial_stock=random.randint(50, 1000))
 
 
 @pytest.fixture
 def cashdesk():
     from c6sh.core.models import Cashdesk
     fake = Factory.create('en-US')
-    return Cashdesk(name='Cashdesk {}'.format(random.randint(0, 10)),
-                    ip_address=fake.ipv4(),
-                    is_active=True)
+    return Cashdesk.objects.create(name='Cashdesk {}'.format(random.randint(0, 10)),
+                                   ip_address=fake.ipv4(),
+                                   is_active=True)
 
 
 @pytest.fixture
 def cashdesk_session_before(cashdesk, user, superuser):
     from c6sh.core.models import CashdeskSession, CashdeskSessionItem
     fake = Factory.create('en-US')
-    cd = CashdeskSession(cashdesk=cashdesk,
-                         user=user,
-                         start=fake.date_time_this_month(),
-                         cash_before=random.choice([50*i for i in range(6)]),
-                         backoffice_user_before=superuser)
+    cd = CashdeskSession.objects.create(cashdesk=cashdesk,
+                                        user=user,
+                                        start=fake.date_time_this_month(),
+                                        cash_before=random.choice([50*i for i in range(6)]),
+                                        backoffice_user_before=superuser)
 
     items = [item() for _ in range(3)]
-    c = [CashdeskSessionItem(session=cd, item=i, amount_before=random.randint(1, i.initial_stock)) for i in items]
+    for item in items:
+        CashdeskSessionItem.objects.create(session=cd,
+                                           item=item,
+                                           amount_before=random.randint(1, item.initial_stock))
     return cd
 
 
 @pytest.fixture
 def quota():
     from c6sh.core.models import Quota
-    return Quota(name='Day {} Quota'.format(random.randint(0, 4)),
-                 size=random.randint(50, 300))
+    return Quota.objects.create(name='Day {} Quota'.format(random.randint(0, 4)),
+                                size=random.randint(50, 300))
 
 
 @pytest.fixture
@@ -76,7 +80,9 @@ def time_constraint_active():
     fake = Factory.create('en-US')
     start = fake.date_time_between(start='-23h', end='-1h')
     end = start + datetime.timedelta(hours=1)
-    return TimeConstraint(name='Active Time Constraint', start=start, end=end)
+    return TimeConstraint.objects.create(name='Active Time Constraint',
+                                         start=start,
+                                         end=end)
 
 
 @pytest.fixture
@@ -85,4 +91,67 @@ def time_constraint_passed():
     fake = Factory.create('en-US')
     start = fake.date_time_between(start='-23h', end='-10h')
     end = fake.date_time_between(start='-9h', end='-2h')
-    return TimeConstraint(name='Passed Time Constraint', start=start, end=end)
+    return TimeConstraint.objects.create(name='Passed Time Constraint',
+                                         start=start,
+                                         end=end)
+
+
+@pytest.fixture
+def warning_constraint():
+    from c6sh.core.models import WarningConstraint
+    return WarningConstraint.objects.create(message='Does the person in front of you have a beard?')
+
+
+@pytest.fixture
+def list_constraint_with_entries():
+    from c6sh.core.models import ListConstraint, ListConstraintEntry
+    fake = Factory.create('en-US')
+    l = ListConstraint.objects.create(name='VIP-Liste!!')
+    [ListConstraintEntry.objects.create(list=l,
+                                        name=fake.name(),
+                                        identifier=random.randint(200, 1000))
+        for _ in range(3)]
+    return l
+
+
+@pytest.fixture
+def product_without_items():
+    from c6sh.core.models import Product
+    fake = Factory.create('en-US')
+    return Product.objects.create(name=fake.catch_phrase(),
+                                  price=random.choice(50*i for i in range(5)),
+                                  tax_rate=19)
+
+
+@pytest.fixture
+def product(product_without_items):
+    product_without_items.items.add(item())
+    return product_without_items
+
+
+@pytest.fixture
+def preorder_unpaid():
+    from c6sh.core.models import Preorder
+    return Preorder.objects.create(order_code=''.join(random.choice(string.ascii_letters) for _ in range(24)))
+
+
+@pytest.fixture
+def preorder_paid(preorder_unpaid):
+    preorder_unpaid.is_paid = True
+    return preorder_unpaid
+
+
+@pytest.fixture
+def preorder_position_paid(preorder_paid, product):
+    from c6sh.core.models import PreorderPosition
+    return PreorderPosition.objects.create(preorder=preorder_paid,
+                                           secret=''.join(random.choice(string.ascii_letters) for _ in range(24)),
+                                           product=product)
+
+
+@pytest.fixture
+def preorder_position_unpaid(preorder_unpaid):
+    from c6sh.core.models import PreorderPosition
+    return PreorderPosition.objects.create(preorder=preorder_unpaid,
+                                           secret=''.join(random.choice(string.ascii_letters) for _ in range(24)),
+                                           product=product)
