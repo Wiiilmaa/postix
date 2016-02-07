43c299ba87fa6dae6166446f3db54aa061aa5037
Raphael Michel <mail@raphaelmichel.de>
2016-02-07 12:37:59 +0100

    Do not return search results for queries of less than 6 characters

--- src/c6sh/api/views.py
+++ src/c6sh/api/views.py
@@ -18,7 +18,8 @@ class PreorderPositionViewSet(ReadOnlyModelViewSet):
 
     You can filter using query parameters by the ``secret`` field. You can also
     search for secrets by using the ``?search=`` query parameter, that will only
-    match the *beginning* of secrets.
+    match the *beginning* of secrets and only if the search query has more than
+    6 characters.
     """
     queryset = PreorderPosition.objects.all()
     serializer_class = PreorderPositionSerializer
@@ -27,6 +28,6 @@ class PreorderPositionViewSet(ReadOnlyModelViewSet):
     def get_queryset(self):
         queryset = PreorderPosition.objects.all()
         search_param = self.request.query_params.get('search', None)
-        if search_param is not None:
+        if search_param is not None and len(search_param) >= 6:
             queryset = queryset.filter(secret__startswith=search_param)
         return queryset
