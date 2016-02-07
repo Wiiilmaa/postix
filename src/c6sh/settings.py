558cd62172bf37261bc63ecf706258dac7a4a84c
Raphael Michel <mail@raphaelmichel.de>
2016-02-07 11:33:16 +0100

    Installed django-rest-framework

--- src/c6sh/settings.py
+++ src/c6sh/settings.py
@@ -17,8 +17,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'c6sh.core',
-    'c6sh.desk'
+    'c6sh.desk',
+    'c6sh.api',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -75,4 +77,10 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
     messages.WARNING: 'warning',
     50: 'critical',
+}
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGE_SIZE': 25
 }
--- src/c6sh/urls.py
+++ src/c6sh/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from .desk import urls as deskurls
+from .api import urls as apiurls
 
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include(apiurls, namespace='desk')),
     url(r'', include(deskurls, namespace='desk')),
 ]
