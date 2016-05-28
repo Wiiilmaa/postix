ae732b99dec8154200842999b6b414c506af0797
Raphael Michel <mail@raphaelmichel.de>
2016-05-28 21:24:12 +0200

    Added product loading to desk UI

--- src/c6sh/static/desk/js/c6sh.js
+++ src/c6sh/static/desk/js/c6sh.js
@@ -1,4 +1,63 @@
+/*global Bloodhound, $*/
+var productlist = {
+    _load_list: function (url) {
+        $.getJSON(url, function(data) {
+            var i, product;
+            for (i = 0; i < data.results.length; i++) {
+                product = data.results[i];
+                if (product.is_available) {
+                    $("<div>").addClass("product").append(
+                        $("<button>").addClass("btn btn-default btn-block").append(
+                            $("<strong>").text(product.name)
+                        ).append($("<br>")).append(product.price + ' €')
+                    ).appendTo($("#product-view"));
+                }
+            }
+            if (data.next !== null) {
+                productlist._load_list(data.next);
+            }
+        });
+    },
+    load_all: function() {
+        productlist._load_list('/api/products/');
+    },
+    init: function () {
+        $("#product-view").html("");
+        productlist.load_all();
+    }
+};
+
+var transaction = {
+    add_product: function () {
+
+    },
+    perform: function () {
+
+    },
+    remove: function () {
+
+    },
+    clear: function () {
+
+    },
+    init: function () {
+
+    }
+};
+
+var confirm_dialog = {
+    show_list_input: function (message, listid, field_name) {
+
+    },
+    show_confirmation: function (message, field_name) {
+
+    }
+};
+
+
 $(function () {
+    productlist.init();
+    /*
     $('#btn-checkout').mousedown(function() {
         $('#lower-right').addClass('post-sale');
     });
@@ -23,9 +82,9 @@ $(function () {
             secrets = [];
             for(var preorder in results){
                 secrets.push(
-                    {   
+                    {
                         value: results[preorder].secret,
-                        count: 1 
+                        count: 1
                     });
             }
             return secrets;
@@ -39,4 +98,5 @@ $(function () {
       minLength: 5,
       source: preSaleTickets
     });
+    */
 });
