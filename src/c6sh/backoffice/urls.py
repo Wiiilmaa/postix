dbfd7119f5c624c356bf45f8922409196457c1be
Tobias Kunze <rixx@cutebit.de>
2016-05-29 21:01:43 +0200

    [backoffice] create_user view and form, zero styling yet
    Dome, Laura and I talked about localization and decided to write the
    entire backoffice app in German.

--- src/c6sh/backoffice/urls.py
+++ src/c6sh/backoffice/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url('^login/', views.LoginView.as_view(), name='login'),
     url('^logout/', views.logout_view, name='logout'),
+    url('^create_user/', views.create_user_view, name='create-user'),
     url('^$', views.main_view, name='main'),
 ]
--- src/c6sh/backoffice/views.py
+++ src/c6sh/backoffice/views.py
@@ -1,8 +1,10 @@
+from django import forms
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import redirect, render
 from django.views.generic import TemplateView
+from ..core.models import User
 
 
 def backoffice_user(user):
@@ -44,3 +46,25 @@ def logout_view(request):
 @backoffice_user_required
 def main_view(request):
     return render(request, 'backoffice/main.html')
+
+
+class CreateUserForm(forms.Form):
+    username = forms.CharField(label='Name')
+    password = forms.CharField(widget=forms.PasswordInput(), label='Passwort')
+    is_backoffice_user = forms.BooleanField(label='Hinterzimmer-Rechte', required=False)
+
+
+@backoffice_user_required
+def create_user_view(request):
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            if form.cleaned_data.pop('is_backoffice_user'):
+                new_user = User.objects.create_superuser(**form.cleaned_data)
+            else:
+                new_user = User.objects.create_user(**form.cleaned_data)
+            return redirect('backoffice:main')
+    else:
+        form = CreateUserForm()
+
+    return render(request, 'backoffice/create_user.html', {'form': form})
