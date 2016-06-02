74bcbe56c0391e5f3273c26ee11898ed90c84677
Tobias Kunze <rixx@cutebit.de>
2016-06-03 00:25:35 +0200

    correct urls passing pks

--- src/c6sh/backoffice/urls.py
+++ src/c6sh/backoffice/urls.py
@@ -3,16 +3,16 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^login/', views.LoginView.as_view(), name='login'),
-    url('^logout/', views.logout_view, name='logout'),
+    url('^login/$', views.LoginView.as_view(), name='login'),
+    url('^logout/$', views.logout_view, name='logout'),
 
-    url('^create_user/', views.create_user_view, name='create-user'),
+    url('^create_user/$', views.create_user_view, name='create-user'),
 
-    url('^session/new/', views.new_session, name='new-session'),
-    url('^session/<pk>/edit/', views.edit_session, name='edit-session'),
-    url('^session/<pk>/resupply/', views.resupply_session, name='resupply-session'),
-    url('^session/<pk>/end/', views.end_session, name='end-session'),
-    url('^session/', views.SessionListView.as_view(), name='session-list'),
+    url('^session/new/$', views.new_session, name='new-session'),
+    url('^session/(?P<pk>[0-9]+)/edit/$', views.edit_session, name='edit-session'),
+    url('^session/(?P<pk>[0-9]+)/resupply/$', views.resupply_session, name='resupply-session'),
+    url('^session/(?P<pk>[0-9]+)/end/$', views.end_session, name='end-session'),
+    url('^session/$', views.SessionListView.as_view(), name='session-list'),
 
     url('^$', views.main_view, name='main'),
 ]
