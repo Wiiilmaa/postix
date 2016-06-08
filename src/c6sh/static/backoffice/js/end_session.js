48d9efe72701fbdfbb59fe435df02b3766efb2df
Tobias Kunze <rixx@cutebit.de>
2016-06-08 23:12:00 +0200

    continued work on end-session view
    done:
        - table with correct(?) data on transactions and starting point
        - make it a form
        - dynamic difference calculation
    
    todo:
        - hidden/hideable lists for presale/sale/reversals/itemmovements
        - pdf generation
        - print
        - styling

---
+++ src/c6sh/static/backoffice/js/end_session.js
@@ -0,0 +1,24 @@
+var table_body = $('#session-items_table tbody');
+
+var get_difference = function() {
+	var row = $(this).closest("tr");
+    var after = parseFloat($(this).val());
+    var before = parseFloat(row.find(".before-value").text());
+	var transaction = parseFloat(row.find(".transaction-value").text());
+	var difference = (before - transaction) - after;
+
+	result_cell = row.find(".after-value");
+	result_cell.html(difference);
+	if (difference != 0) {
+		result_cell.addClass("danger");
+		result_cell.removeClass("success");
+    } else {
+		result_cell.removeClass("danger");
+		result_cell.addClass("success");
+    }
+}
+
+$('.numberinput').each(function(index) {
+    console.log(this);
+    $(this).on('keyup', get_difference);
+})
