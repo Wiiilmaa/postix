2b1bf283e402c1a0383b7168232438acc33e568b
Tobias Kunze <rixx@cutebit.de>
2016-08-27 18:16:11 +0200

    [core] Log inputs to DummyPrinter

--- src/c6sh/core/utils/printing.py
+++ src/c6sh/core/utils/printing.py
@@ -1,3 +1,4 @@
+import logging
 import subprocess
 import time
 from collections import defaultdict
@@ -34,11 +35,8 @@ class CashdeskPrinter:
     def cut_tape(self):
         self.send(bytearray([0x1D, 0x56, 66, 100]))
 
-    def print_receipt(self, transaction, do_open_drawer=True):
+    def _build_receipt(self, transaction):
         settings = EventSettings.objects.get()
-        if do_open_drawer:
-            self.open_drawer()
-
         total_sum = 0
         position_lines = list()
         tax_sums = defaultdict(int)
@@ -50,7 +48,7 @@ class CashdeskPrinter:
             total_sum += position.value
             tax_sums[position.tax_rate] += position.tax_value
             if position.tax_rate not in tax_symbols:
-                tax_symbols[position.tax_rate] = ascii_uppercase[len(tax_sums)]
+                tax_symbols[position.tax_rate] = ascii_uppercase[len(tax_sums) - 1]
             pos_str = ' {product_name} ({tax_str}){gap} {price}'.format(
                 product_name=position.product.name,
                 tax_str=tax_symbols[position.tax_rate],
@@ -64,16 +62,17 @@ class CashdeskPrinter:
             return
 
         receipt = str(bytearray([0x1B, 0x61, 1]))
-        receipt += settings.receipt_address
+        receipt += settings.receipt_address + '\r\n'
         receipt += SEPARATOR
         receipt += POSITION_LIST_HEADER
 
         receipt += '\r\n'.join(position_lines)
+        receipt += '\r\n'
         receipt += SEPARATOR
         receipt += "                      Nettosumme:  {}\r\n".format(self._format_number(total_sum - total_taxes))
 
         for tax in sorted(list(tax_symbols))[::-1]:
-            receipt += "                    MwSt {tax_rate}%% ({tax_identifier}):  {tax_amount}\r\n".format(
+            receipt += "                 MwSt {tax_rate}% ({tax_identifier}):  {tax_amount}\r\n".format(
                 tax_rate=tax,
                 tax_identifier=tax_symbols[tax],
                 tax_amount=self._format_number(tax_sums[tax]),
@@ -82,30 +81,39 @@ class CashdeskPrinter:
         receipt += "                           Summe:  {}\r\n".format(self._format_number(total_sum))
         receipt += settings.receipt_footer + '\r\n'
         receipt += '{} {}\r\n'.format(
-            transaction.timestamp.strftime("%d.%m.%Y %H:%M"),
+            transaction.datetime.strftime("%d.%m.%Y %H:%M"),
             transaction.session.cashdesk.name,
         )
         receipt += 'Belegnummer: {}\r\n'.format(transaction.pk)
         receipt += '\r\n\r\n'
+        return receipt
+
+    def print_receipt(self, transaction, do_open_drawer=True):
+        if do_open_drawer:
+            self.open_drawer()
+        receipt = self._build_receipt(transaction)
 
         try:
             # self.send(image_tools.get_imagedata(settings.STATIC_ROOT + '/' + settings.EVENT_RECIPE_HEADER))
             self.send(receipt)
             self.cut_tape()
         except:
-            print(receipt)
             pass
 
 
 class DummyPrinter:
+    def __init__(self, *args, **kwargs):
+        self.logger = logging.getLogger('django')
+
     def send(self, data):
-        pass
+        self.logger.info('[DummyPrinter] Received data: {}'.format(data))
 
     def open_drawer(self):
-        pass
+        self.logger.info('[DummyPrinter] Opened drawer')
 
     def cut_tape(self):
-        pass
+        self.logger.info('[DummyPrinter] Cut tape')
 
     def print_receipt(self, transaction, do_open_drawer=True):
-        pass
+        receipt = CashdeskPrinter('')._build_receipt(transaction)
+        self.logger.info('[DummyPrinter] Printed receipt:\n{}'.format(receipt))
