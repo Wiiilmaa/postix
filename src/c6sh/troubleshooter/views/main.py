ea1dce2b64cb3d499dcc4e6758813816f1077ab7
Raphael Michel <mail@raphaelmichel.de>
2016-08-29 15:53:06 +0200

    [troubleshooter] Session overview on dashboard

--- src/c6sh/troubleshooter/views/main.py
+++ src/c6sh/troubleshooter/views/main.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
 from .utils import troubleshooter_user_required
+from ...core.models import Cashdesk
 
 
 @troubleshooter_user_required
 def main_view(request):
-    return render(request, 'troubleshooter/main.html', {})
+    ctx = {}
+
+    sessions = []
+    for c in Cashdesk.objects.filter(is_active=True).order_by('name'):
+        for sess in c.get_active_sessions():
+            sess.current_items = sess.get_current_items()
+            sessions.append(sess)
+
+    ctx['sessions'] = sessions
+
+    return render(request, 'troubleshooter/main.html', ctx)
