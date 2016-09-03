f22ec024b5f50665b97dd0fbe11fc05d58ffb4b2
kassenserver <kassenserver>
2016-09-03 15:07:10 +0200

    [core] Added minimal stats via

---
+++ src/c6sh/core/management/commands/stats.py
@@ -0,0 +1,22 @@
+import decimal
+import json
+import os
+
+from django.core.management.base import BaseCommand
+from django.db.models import Sum, Count
+from c6sh.core.models import Cashdesk, Preorder, PreorderPosition, Product, TransactionPosition
+ 
+
+class Command(BaseCommand):
+    help = 'Print a view stats'
+
+    def handle(self, *args, **kwargs):
+        agg = TransactionPosition.objects.order_by('product').values("product__name", "product__price").annotate(c=Count('id'), r=Count('reverses'))
+        total = 0
+        for line in agg:
+            s = line['c']-line['r']
+            print("{l[product__name]:30} {l[product__price]:>20} EUR       {s}".format(l=line, s=s))
+            if 'ticket' in line['product__name']:
+                total += s
+
+        print("Total tickets: {total}".format(total=total))
