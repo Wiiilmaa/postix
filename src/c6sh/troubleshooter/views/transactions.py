1042aa44c2eb542462b7f9acb53dbf80c79e1cbf
Tobias Kunze <rixx@cutebit.de>
2016-10-15 22:56:49 +0200

    [troubleshooter] very WIP: Transaction list view

---
+++ src/c6sh/troubleshooter/views/transactions.py
@@ -0,0 +1,40 @@
+from django.views.generic.list import ListView
+
+from .utils import TroubleshooterUserRequiredMixin
+from ...core.models import Cashdesk, Transaction, TransactionPosition
+
+
+class TransactionListView(TroubleshooterUserRequiredMixin, ListView):
+    model = Transaction
+    template_name = 'troubleshooter/transaction_list.html'
+    context_object_name = 'transactions'
+    paginate_by = 50
+
+    def dispatch(self, request, *args, **kwargs):
+        self.filter = dict()
+        _filter = self.request.GET
+
+        if 'type' in _filter:
+            print(_filter['type'])
+            self.filter['type'] = [
+                request_filter for request_filter in _filter['type']
+                if request_filter in (t[0] for t in TransactionPosition.TYPES)
+            ]
+                
+        if 'desk' in _filter:
+            try:
+                desk = Cashdesk.objects.get(pk=_filter['cashdesk'])
+                self.filter['cashdesk'] = desk
+            except Cashdesk.DoesNotExist:
+                pass
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_queryset(self):
+        qs = TransactionPosition.objects.all()
+        if 'cashdesk' in self.filter:
+            qs = qs.filter(transaction__cashdesk=self.filter['cashdesk'])
+        if 'type' in self.filter and self.filter['type']:
+            qs = qs.filter(type__in=self.filter['type'])
+        # TODO: return Transactions?
+        # TODO: prefetches
+        return qs
