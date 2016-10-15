93b66d2c1abd2523c3a7f58ee53b8f11284936d3
Raphael Michel <mail@raphaelmichel.de>
2016-10-15 13:01:33 +0200

    [desk] Remove unused methods

--- src/c6sh/static/desk/js/keyboard.js
+++ src/c6sh/static/desk/js/keyboard.js
@@ -1,19 +1,5 @@
 var keyboard = {
 
-    _escape: function () {
-        transaction.clear();
-    },
-
-    _delete: function () {
-        transaction.remove(transaction.positions.length - 1);
-    },
-    
-    _enter: function () {
-        if (transaction.positions.length > 0 && !transaction.post_sale) {
-            transaction.perform();
-        }
-    },
-
     init: function () {
         $('body').on('keyup', function (e) {
             var tag = e.target.tagName.toLowerCase(),
