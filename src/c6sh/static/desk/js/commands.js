5d7ac1788053b31a8620fc953cc52fa3779377e6
Raphael Michel <mail@raphaelmichel.de>
2016-10-15 13:23:01 +0200

    [desk] Added command for scanner configuration

--- src/c6sh/static/desk/js/commands.js
+++ src/c6sh/static/desk/js/commands.js
@@ -6,11 +6,30 @@ var commands = {
     },
 
     '/help': function () {
-
         commands._info_view("<p><strong>Supported commands:</strong></p>"
             + "<dl class='dl-horizontal'>"
             + "<dt>/scanner</dt><dd>Show reset codes for the barcode scanner</dd>"
             + "</dl>");
+    },
+
+    '/scanner': function () {
+        commands._info_view("<p><strong>Scan the following, in order:</strong></p>"
+            + "<p>"
+            + "<img src='/static/c6sh/desk/img/scanner/honeywell01.png'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+            + "<img src='/static/c6sh/desk/img/scanner/honeywell02.png'>"
+            + "</p>"
+            + "<p>"
+            + "<img src='/static/c6sh/desk/img/scanner/honeywell03.png'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+            + "<img src='/static/c6sh/desk/img/scanner/honeywell04.png'>"
+            + "</p>"
+            + "<p>"
+            + "<img src='/static/c6sh/desk/img/scanner/honeywell05.png'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+            + "<img src='/static/c6sh/desk/img/scanner/honeywell06.png'>"
+            + "</p>"
+            + "<p>"
+            + "<img src='/static/c6sh/desk/img/scanner/honeywell07.png'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+            + "<img src='/static/c6sh/desk/img/scanner/honeywell08.png'>"
+            + "</p>");
     },
 
     process: function (command) {
