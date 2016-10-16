0029d661b597a7c53ea9d12406d326970fcc31f4
Tobias Kunze <rixx@cutebit.de>
2016-10-16 21:44:08 +0200

    [troubleshooter] Add ListConstraintEntriesFilter

--- src/c6sh/troubleshooter/views/constraints.py
+++ src/c6sh/troubleshooter/views/constraints.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
@@ -15,3 +16,16 @@ class ListConstraintDetailView(TroubleshooterUserRequiredMixin, DetailView):
     template_name = 'troubleshooter/constraint_detail.html'
     context_object_name = 'constraint'
     model = ListConstraint
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        obj = kwargs['object']
+
+        if self.request.GET and self.request.GET['filter'] and self.request.GET['filter'][0]:
+            query = self.request.GET['filter'][0]
+            ctx['entries'] = obj.entries.filter(
+                Q(name__icontains=query) | Q(identifier__icontains=query)
+            )
+        else:
+            ctx['entries'] = obj.entries.all()
+        return ctx
