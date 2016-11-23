463a1e46104ba6a74597a89a45fcebb5489e46e3
Tobias Kunze <rixx@cutebit.de>
2016-11-23 10:17:27 +0100

    [backoffice] Make wizard form usable

--- src/c6sh/backoffice/views/wizard.py
+++ src/c6sh/backoffice/views/wizard.py
@@ -3,11 +3,25 @@ from django.views.generic import FormView
 
 from c6sh.backoffice.forms import EventSettingsForm
 from c6sh.backoffice.views.utils import BackofficeUserRequiredMixin
+from c6sh.core.models import EventSettings
 
 
 class WizardSettingsView(BackofficeUserRequiredMixin, FormView):
     template_name = 'backoffice/wizard_settings.html'
     form_class = EventSettingsForm
+
+    def post(self, request):
+        form = self.get_form()
+        if form.is_valid():
+            form.save()
+            return self.form_valid(form)
+
+    def get_initial(self):
+        settings = EventSettings.objects.get_solo()
+        return {
+            attr: getattr(settings, attr)
+            for attr in EventSettingsForm().fields
+        }
 
     def get_success_url(self):
         return reverse('backoffice:main')
