345523352df799d045e2b3839a75217fdf712e87
Tobias Kunze <rixx@cutebit.de>
2016-11-23 09:17:37 +0100

    [backoffice] Move forms to directory

---
+++ src/c6sh/backoffice/forms/__init__.py
@@ -0,0 +1,11 @@
+from .session import (
+    ItemMovementForm, ItemMovementFormSetHelper, SessionBaseForm,
+    get_form_and_formset,
+)
+
+__all__ = [
+    'get_form_and_formset',
+    'ItemMovementForm',
+    'ItemMovementFormSetHelper',
+    'SessionBaseForm',
+]
