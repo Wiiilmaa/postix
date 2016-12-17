49019a0ab014a0006d8b3adee141e4906cd415d9
Tobias Kunze <rixx@cutebit.de>
2016-12-17 16:35:53 +0100

    Remove LocaleMiddleware; use configured locale

--- src/c6sh/settings.py
+++ src/c6sh/settings.py
@@ -52,7 +52,6 @@ except ImportError:
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
