84c5eadf91546d14b6541df8593a2d7bf07cb671
Tobias Kunze <rixx@cutebit.de>
2016-12-20 12:59:02 +0100

    [core] Add QR generation to Ping model

--- src/c6sh/core/models/ping.py
+++ src/c6sh/core/models/ping.py
@@ -1,3 +1,6 @@
+from tempfile import TemporaryFile
+
+import qrcode
 from django.db import models
 from django.utils.crypto import get_random_string
 
@@ -10,3 +13,17 @@ class Ping(models.Model):
     pinged = models.DateTimeField(auto_now_add=True)
     ponged = models.DateTimeField(null=True, blank=True)
     secret = models.CharField(max_length=20, default=generate_ping_secret)
+
+    def get_qr_code(self):
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=5,
+            border=4,
+        )
+        qr.add_data(self.secret)
+        qr.make()
+        f = TemporaryFile()
+        img = qr.make_image()
+        img.save(f)
+        return f
