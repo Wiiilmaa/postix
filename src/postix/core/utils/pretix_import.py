548a2606289e0d2d4c95c533b9f9334750d8f8ce
Tobias Kunze <rixx@cutebit.de>
2017-08-24 20:02:41 +0200

    [core] Move management command guts to helper

---
+++ src/postix/core/utils/pretix_import.py
@@ -0,0 +1,99 @@
+import json
+from decimal import Decimal
+
+from postix.core.models import Cashdesk, Preorder, PreorderPosition, Product
+
+
+def fake_style(string):
+    return string
+
+
+class FakeLog:
+    def write(self, string):
+        print(string)
+
+
+def _build_product_dict(data, log, style):
+    created_items = 0
+    loaded_items = 0
+    product_dict = dict()
+    for item in data['items']:
+        if item['variations']:
+            log.write(style.ERROR('Warning: Import script cannot deal with variations yet!'))
+
+        try:
+            product = Product.objects.get(import_source_id=item['id'])
+            loaded_items += 1
+        except Product.DoesNotExist:
+            product = Product(import_source_id=item['id'])
+            product.price = Decimal(item['price'])
+            product.tax_rate = Decimal(item['tax_rate'])
+            created_items += 1
+        product.name = item['name']
+        product.save()
+        product_dict[item['id']] = product
+
+    log.write(style.SUCCESS(
+        'Found {} new and {} known products in file.'.format(created_items, loaded_items)
+    ))
+    return product_dict
+
+
+def import_pretix_data(data, add_cashdesks=False, log=FakeLog(), style=fake_style):
+
+    presale_export = json.load(data)['event']
+    log.write(style.NOTICE(
+        'Importing data from event "{}".'.format(presale_export['name'])
+    ))
+
+    orders = presale_export['orders']
+    product_dict = _build_product_dict(presale_export, log=log, style=style)
+
+    created_orders = 0
+    loaded_orders = 0
+    for order in orders:
+        preorder, created = Preorder.objects.get_or_create(
+            order_code=order['code']
+        )
+        preorder.is_paid = (order['status'] == 'p')
+        preorder.save()
+
+        if not created:
+            preorder_positions = {
+                p.secret: p for p in preorder.positions.all()
+            }
+        else:
+            preorder_positions = {}
+
+        for position in order['positions']:
+            if position['secret'] in preorder_positions:
+                pp = preorder_positions[position['secret']]
+                del preorder_positions[position['secret']]
+            else:
+                pp = PreorderPosition(preorder=preorder, secret=position['secret'])
+            pp.product = product_dict[position['item']]
+            pp.save()
+
+        if preorder_positions:
+            for pp in preorder_positions.values():
+                pp.delete()
+
+        created_orders += int(created)
+        loaded_orders += int(not created)
+
+    log.write(style.SUCCESS(
+        'Found {} new and {} known orders in file.'.format(created_orders, loaded_orders)
+    ))
+
+    if add_cashdesks:
+        for cashdesk_number in range(5):
+            Cashdesk.objects.get_or_create(
+                name='Cashdesk {}'.format(cashdesk_number + 1),
+                ip_address='127.0.0.{}'.format(cashdesk_number + 1),
+            )
+        log.write(style.SUCCESS(
+            'Added 5 cashdesks.'
+        ))
+    log.write(style.SUCCESS(
+        'Import done.'
+    ))
