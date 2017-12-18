9f077c46b1378079a45c7189038a09b35188ae40
Tobias Kunze <rixx@cutebit.de>
2017-12-18 03:30:25 +0100

    [core] Add method for attendance list printing

--- src/postix/core/utils/printing.py
+++ src/postix/core/utils/printing.py
@@ -5,7 +5,7 @@ import time
 from collections import defaultdict
 from decimal import Decimal
 from string import ascii_uppercase
-from typing import Union
+from typing import List, Union
 
 from django.utils import timezone
 from django.utils.translation import ugettext as _
@@ -13,6 +13,7 @@ from PIL import Image
 
 from postix.core.models import Transaction
 
+SEPARATOR_CHAR = '\u2500'
 SEPARATOR = '\u2500' * 42 + '\r\n'
 
 
@@ -164,6 +165,64 @@ class CashdeskPrinter:
             except Exception as e:
                 logging.getLogger('django').exception('Printing at {} failed: {}'.format(self.printer, str(e)))
 
+    def _build_attendance(self, arrived: List['PreorderPosition'], not_arrived: List['PreorderPosition']):
+        def shorten(line):
+            if len(line) <= 40:
+                return line
+            return line[:39] + '...'
+
+        def build_lines(position):
+            information_lines = ['  ' + shorten(l.strip()) for l in position.information.split('\n') if l.strip()]
+            if information_lines:
+                information_lines[0] = '-' + information_lines[0][1:]
+            return information_lines
+
+        from postix.core.models import EventSettings
+        settings = EventSettings.get_solo()
+        arrived_lines = [build_lines(position) for position in arrived]
+        not_arrived_lines = [build_lines(position) for position in not_arrived]
+
+        attendance = bytearray([self.ESC, 0x61, 1]).decode()  # center text
+        attendance += bytearray([self.ESC, 0x45, 1]).decode()  # emphasize
+        attendance += settings.name + ' ' + _('Attendance') + '\r\n'
+        attendance += timezone.now().strftime("%Y-%m-%d %H:%M") + '\r\n'
+        attendance += bytearray([self.ESC, 0x45, 0]).decode()  # de-emphasize
+
+        if arrived_lines:
+            arrived_lines = sum(arrived_lines, [])
+            attendance += bytearray([self.ESC, 0x61, 1]).decode()  # center text
+            attendance += '\r\n'
+            count = '{count} '.format(count=len(arrived))
+            seplen = (40 - len(count + _('Arrived'))) // 2
+            attendance += SEPARATOR_CHAR * seplen + ' ' + count + _('Arrived') + ' ' + SEPARATOR_CHAR * seplen + '\r\n'
+            attendance += bytearray([self.ESC, 0x61, 2]).decode()  # right-align text (0 would be left-align)
+            attendance += '\r\n'.join(arrived_lines)
+            attendance += '\r\n'
+
+        if not_arrived_lines:
+            not_arrived_lines = sum(not_arrived_lines, [])
+            attendance += bytearray([self.ESC, 0x61, 1]).decode()  # center text
+            attendance += '\r\n'
+            count = '{count} '.format(count=len(not_arrived))
+            seplen = (40 - len(count + _('Not arrived'))) // 2
+            attendance += SEPARATOR_CHAR * seplen + ' ' + count + _('Not arrived') + ' ' + SEPARATOR_CHAR * seplen + '\r\n'
+            attendance += bytearray([self.ESC, 0x61, 2]).decode()  # right-align text (0 would be left-align)
+            attendance += '\r\n'.join(not_arrived_lines)
+            attendance += '\r\n'
+
+        attendance += bytearray([self.ESC, 0x61, 1]).decode()  # center text
+        attendance += '\r\n'
+        return attendance
+
+    def print_attendance(self, arrived: List['PreorderPosition'], not_arrived: List['PreorderPosition']):
+        attendance = self._build_attendance(arrived, not_arrived)
+        if attendance:
+            try:
+                self.send(attendance)
+                self.cut_tape()
+            except Exception as e:
+                logging.getLogger('django').exception('Printing at {} failed: {}'.format(self.printer, str(e)))
+
     def print_text(self, text: str, centered=True, cut_tape=True) -> None:
         center = bytearray([self.ESC, 0x61, 1]).decode()  # center text
         left_align = bytearray([self.ESC, 0x61, 0]).decode()  # left-align text (0 would be left-align)
@@ -236,6 +295,12 @@ class DummyPrinter:
         if receipt is not None:
             self.logger.info('[DummyPrinter] Printed receipt:\n{}'.format(receipt))
         return receipt
+
+    def print_attendance(self, arrived: List['PreorderPosition'], not_arrived: List['PreorderPosition']):
+        arrivals = CashdeskPrinter('', self.cashdesk)._build_attendance(arrived, not_arrived)
+        if arrivals is not None:
+            self.logger.info('[DummyPrinter] Printed arrivals:\n{}'.format(arrivals))
+        return arrivals
 
     def print_image(self, fileish):
         self.logger.info('[DummyPrinter] Printed image')
