b5f7a01c80085440f1137c6213887a960064e961
Tobias Kunze <rixx@cutebit.de>
2017-12-23 08:39:04 +0100

    [core] Import only one member list at a time

--- src/postix/core/management/commands/import_member.py
+++ src/postix/core/management/commands/import_member.py
@@ -6,43 +6,37 @@ from postix.core.models import ListConstraint, ListConstraintEntry
 
 
 class Command(BaseCommand):
-    help = 'Invocation: import_member ~/list_global.csv ~/list_local.csv BLN'
+    help = 'Invocation: import_member ~/member_list.csv [BLN]'
 
     def add_arguments(self, parser):
-        parser.add_argument('list_global')
-        parser.add_argument('list_local')
-        parser.add_argument('local_prefix')
+        parser.add_argument('member_list')
+        parser.add_argument('local_prefix', default='')
 
     def handle(self, *args, **kwargs):
         constraints, _ = ListConstraint.objects.get_or_create(confidential=True, name='Mitglieder')
-        import_count_global = import_known_global = 0
-        import_count_local = import_known_local = 0
+        import_count= import_known= 0
         local_prefix = kwargs['local_prefix']
 
-        with open(kwargs['list_global'], 'r') as global_list:
-            reader = csv.DictReader(global_list)
+        with open(kwargs['member_list'], 'r') as member_list:
+            if not local_prefix:
+                reader = csv.DictReader(member_list)
+            else:
+                reader = csv.DictReader(member_list)
             for row in reader:
-                _, created = ListConstraintEntry.objects.get_or_create(
-                    list=constraints,
-                    name='{} {}'.format(row['VORNAME'], row['NACHNAME']),
-                    identifier=row['CHAOSNR'],
-                )
-                if created:
-                    import_count_global += 1
+                if not local_prefix:
+                    _, created = ListConstraintEntry.objects.get_or_create(
+                        list=constraints,
+                        name='{} {}'.format(row['VORNAME'], row['NACHNAME']),
+                        identifier=row['CHAOSNR'],
+                    )
                 else:
-                    import_known_global += 1
-        self.stdout.write(self.style.SUCCESS('Imported {} entries of the global dataset, {} were already known.').format(import_count_global, import_known_global))
-
-        with open(kwargs['list_local'], 'r') as local_list:
-            reader = csv.DictReader(local_list)
-            for row in reader:
-                _, created = ListConstraintEntry.objects.get_or_create(
-                    list=constraints,
-                    name=row['NAME'].strip(),
-                    identifier='{}-{}'.format(local_prefix, row['CHAOSNR']),
-                )
+                    _, created = ListConstraintEntry.objects.get_or_create(
+                        list=constraints,
+                        name=row['NAME'].strip(),
+                        identifier='{}-{}'.format(local_prefix, row['CHAOSNR']),
+                    )
                 if created:
-                    import_count_local += 1
+                    import_count += 1
                 else:
-                    import_known_local += 1
-        self.stdout.write(self.style.SUCCESS('Imported {} entries of the local dataset, {} were already known.').format(import_count_local, import_known_local))
+                    import_known+= 1
+        self.stdout.write(self.style.SUCCESS('Imported {} entries of the dataset, {} were already known.').format(import_count, import_known))
