76c002c71423b3df8c017f7a5548af26e87ca2b8
Tobias Kunze <rixx@cutebit.de>
2018-06-10 13:18:26 +0200

    Add translations

--- src/postix/core/models/record.py
+++ src/postix/core/models/record.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils.timezone import now
+from django.utils.translation import ugettext_lazy as _
 
 
 class RecordEntity(models.Model):
@@ -11,18 +12,19 @@ class RecordEntity(models.Model):
     def __str__(self):
         return '{s.name}: {s.detail}'.format(s=self)
 
+
 class Record(models.Model):
     TYPES = (
-        ('inflow', 'inflow'),
-        ('outflow', 'outflow'),
+        ('inflow', _('Inflow')),
+        ('outflow', _('Outflow')),
     )
-    type = models.CharField(max_length=20, choices=TYPES)
-    datetime = models.DateTimeField()
-    entity = models.ForeignKey(RecordEntity, on_delete=models.PROTECT, related_name='records')
-    carrier = models.CharField(max_length=200, null=True, blank=True)
-    amount = models.DecimalField(decimal_places=2, max_digits=10)
-    backoffice_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='records')
-    is_balancing = models.BooleanField(default=False)
+    type = models.CharField(max_length=20, choices=TYPES, verbose_name=_('Direction'))
+    datetime = models.DateTimeField(verbose_name=_('Date'))
+    entity = models.ForeignKey(RecordEntity, on_delete=models.PROTECT, related_name='records', verbose_name=_('Entity'))
+    carrier = models.CharField(max_length=200, null=True, blank=True, verbose_name=_('Carrier'))
+    amount = models.DecimalField(decimal_places=2, max_digits=10, verbose_name=_('Amount'))
+    backoffice_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='records', verbose_name=_('Backoffice user'))
+    is_balancing = models.BooleanField(default=False, verbose_name=_('Is a balancing record'))
 
     def __str__(self):
         return self.datetime.strftime('Day %d %X') + " " + str(self.entity) + " " + str(self.amount) + " EUR"
