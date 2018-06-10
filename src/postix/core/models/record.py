8d69bfabebf1a9813c49e82261e969393ac0d204
Tobias Kunze <rixx@cutebit.de>
2018-06-10 14:31:12 +0200

    [core] Add ordering to record models

--- src/postix/core/models/record.py
+++ src/postix/core/models/record.py
@@ -9,6 +9,9 @@ class RecordEntity(models.Model):
     name = models.CharField(max_length=200, help_text='For example "Bar", or "Vereinstisch", …')
     detail = models.CharField(max_length=200, help_text='For example the name of the bar, …')
 
+    class Meta:
+        ordering = ('name', 'detail')
+
     def __str__(self):
         return '{s.name}: {s.detail}'.format(s=self)
 
@@ -19,12 +22,15 @@ class Record(models.Model):
         ('outflow', _('Outflow')),
     )
     type = models.CharField(max_length=20, choices=TYPES, verbose_name=_('Direction'))
-    datetime = models.DateTimeField(verbose_name=_('Date'))
-    entity = models.ForeignKey(RecordEntity, on_delete=models.PROTECT, related_name='records', verbose_name=_('Entity'))
+    datetime = models.DateTimeField(verbose_name=_('Date'), help_text=_('Leave empty to use the current date and time.'))
+    entity = models.ForeignKey(RecordEntity, on_delete=models.PROTECT, related_name='records', verbose_name=_('Entity'), null=True, blank=True)
     carrier = models.CharField(max_length=200, null=True, blank=True, verbose_name=_('Carrier'))
     amount = models.DecimalField(decimal_places=2, max_digits=10, verbose_name=_('Amount'))
     backoffice_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='records', verbose_name=_('Backoffice user'))
     is_balancing = models.BooleanField(default=False, verbose_name=_('Is a balancing record'))
+
+    class Meta:
+        ordering = ('datetime', )
 
     def __str__(self):
         return self.datetime.strftime('Day %d %X') + " " + str(self.entity) + " " + str(self.amount) + " EUR"
