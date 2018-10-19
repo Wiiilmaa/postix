0c6c402118d1aeec2a2e40ecc13e5472c3eb3c9d
Tobias Kunze <r@rixx.de>
2018-10-20 00:03:32 +0200

    Re-style sidebar

--- src/postix/troubleshooter/context.py
+++ src/postix/troubleshooter/context.py
@@ -1,3 +1,6 @@
+from django.http import Http404
+from django.urls import resolve
+
 from ..core.models import TroubleshooterNotification
 
 
@@ -5,11 +8,18 @@ def processor(request):
     """
     Adds data to all template contexts
     """
-    if not request.path.startswith('/troubleshooter'):
-        return {}
-
     ctx = {}
+    try:
+        url = resolve(request.path_info)
+        ctx['url_name'] = url.url_name
+        ctx['url_namespace'] = url.namespace
+    except Http404:
+        ctx['url_name'] = ''
+        ctx['url_namespace'] = ''
+
+    if not request.path.startswith('/troubleshooter'):
+        return ctx
+
     if TroubleshooterNotification.objects.active().exists():
         ctx['has_request'] = True
-
     return ctx
