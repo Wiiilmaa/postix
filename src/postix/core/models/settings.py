77e30d6ad0d1799b6a361f8d15ba54af78fda8ff
Tobias Kunze <r@rixx.de>
2018-11-03 16:18:57 +0100

    I18n ALL the settings fields

--- src/postix/core/models/settings.py
+++ src/postix/core/models/settings.py
@@ -1,31 +1,48 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 from solo.models import SingletonModel
 
 
 class EventSettings(SingletonModel):
-    name = models.CharField(max_length=100, default='Generic Event')
+    name = models.CharField(
+        max_length=100, default='Generic Event', verbose_name=_('Name')
+    )
     short_name = models.CharField(
         max_length=50,
         default='GE',
-        help_text='A short name for your event.',
+        verbose_name=_('Short name'),
+        help_text=_('A short name for your event.'),
     )
     support_contact = models.CharField(
         max_length=200,
-        default='Who is flying this thing? Enter your contact information as support contact info, please.',
-        help_text='Your - yes YOUR - real-time contact info, e.g. phone number.',
+        verbose_name=_('Support contact'),
+        default=_(
+            'Who is flying this thing? Enter your contact information as support contact info, please.'
+        ),
+        help_text=_('Your - yes YOUR - real-time contact info, e.g. phone number.'),
     )
-    invoice_address = models.CharField(max_length=200, blank=True, null=True)
-    invoice_footer = models.CharField(max_length=200, blank=True, null=True)
-    receipt_address = models.CharField(max_length=200, blank=True, null=True)
+    invoice_address = models.CharField(
+        verbose_name=_('Invoice address'), max_length=200, blank=True, null=True
+    )
+    invoice_footer = models.CharField(
+        verbose_name=_('Invoice footer'), max_length=200, blank=True, null=True
+    )
+    receipt_address = models.CharField(
+        verbose_name=_('Receipt address'), max_length=200, blank=True, null=True
+    )
     receipt_footer = models.CharField(
+        verbose_name=_('Receipt footer'),
         max_length=200,
-        default='Thank you!',
-        help_text='Use this to display additional disclaimers/data not in your address, such as VAT IDs.',
+        default=_('Thank you!'),
+        help_text=_(
+            'Use this to display additional disclaimers/data not in your address, such as VAT IDs.'
+        ),
     )
     report_footer = models.CharField(
+        verbose_name=_('Report footer'),
         max_length=500,
         default='CCC Veranstaltungsgesellschaft mbH',
-        help_text='This will show up on backoffice session reports.'
+        help_text=_('This will show up on backoffice session reports.'),
     )
     initialized = models.BooleanField(default=False)
 
