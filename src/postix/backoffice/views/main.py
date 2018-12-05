c57641de7dc74cbdeb5bf1f90a15e94824ab3d2f
Tobias Kunze <r@rixx.de>
2018-12-05 17:20:01 +0100

    [backoffice] Show current redemption progress in dashboard

--- src/postix/backoffice/views/main.py
+++ src/postix/backoffice/views/main.py
@@ -2,6 +2,7 @@ from django.views.generic import TemplateView
 
 from postix.core.models import Quota
 from postix.core.models.base import Item, Product
+from postix.core.models.preorder import PreorderPosition
 
 from .. import checks
 from .utils import BackofficeUserRequiredMixin
@@ -16,4 +17,7 @@ class MainView(BackofficeUserRequiredMixin, TemplateView):
         ctx['items'] = Item.objects.all()
         ctx['quotas'] = Quota.objects.all()
         ctx['check_errors'] = checks.all_errors()
+        ctx['all_preorders'] = PreorderPosition.objects.filter(preorder__is_paid=True).count()
+        ctx['redeemed_preorders'] = PreorderPosition.objects.filter(preorder__is_paid=True, transaction_positions__isnull=False).count()
+        ctx['preorder_percentage'] = round(ctx['redeemed_preorders'] * 100 / ctx['all_preorders'], 2)
         return ctx
