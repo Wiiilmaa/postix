af00ccb41471c6fa229bb30e14fbfec5a5f26730
Tobias Kunze <r@rixx.de>
2018-12-07 14:41:12 +0100

    Sync pings in runperiodic command

--- src/postix/core/management/commands/runperiodic.py
+++ src/postix/core/management/commands/runperiodic.py
@@ -1,8 +1,13 @@
 from django.core.management.base import BaseCommand
 
+from postix.core.models import Ping
+
 
 class Command(BaseCommand):
     help = 'Run periodic tasks'
 
     def handle(self, *args, **options):
-        pass
+        for ping in Ping.objects.filter(synced=False, ponged__isnull=False).order_by(
+            'ponged'
+        ):
+            ping.sync()
