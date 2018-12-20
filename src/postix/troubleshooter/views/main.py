9d227f741268716172593dce758e4f69d7c1391e
Raphael Michel <mail@raphaelmichel.de>
2018-12-20 14:23:45 +0100

    [troubleshooter] Show remaining stock

--- src/postix/troubleshooter/views/main.py
+++ src/postix/troubleshooter/views/main.py
@@ -1,5 +1,8 @@
+from django.db.models import Sum
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+
+from postix.core.models.base import ItemSupplyPack
 
 from ...core.models import Cashdesk
 from .utils import troubleshooter_user_required
@@ -16,5 +19,8 @@ def main_view(request: HttpRequest) -> HttpResponse:
             sessions.append(sess)
 
     ctx['sessions'] = sessions
+    ctx['troubleshooter_stock'] = ItemSupplyPack.objects.filter(
+        state='troubleshooter'
+    ).order_by().values('item', 'item__name').annotate(s=Sum('amount'))
 
     return render(request, 'troubleshooter/main.html', ctx)
