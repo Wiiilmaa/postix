7e2a77fa2a6617b410f4116740d7206a1054cb56
Tobias Kunze <r@rixx.de>
2019-01-04 00:01:39 +0100

    Export list of records

---
+++ src/postix/core/management/commands/export_records.py
@@ -0,0 +1,17 @@
+import csv
+import sys
+
+from django.core.management.base import BaseCommand
+
+from postix.core.models import Record
+
+
+class Command(BaseCommand):
+    help = 'Export generated records as csv.'
+
+    def handle(self, *args, **kwargs):
+        keys = [('date', 'Datum'), ('time', 'Uhrzeit'), ('direction', 'Richtung'), ('amount', 'Betrag'), ('entity', 'Quelle/Ziel'), ('entity_detail', 'Detail'), ('supervisor', 'Person'), ('user', 'Einlieferer/Empfänger'), ('checksum', 'Prüfsumme')]
+        writer = csv.DictWriter(sys.stdout, fieldnames=[k[0] for k in keys])
+        writer.writerow(dict(keys))
+        for record in Record.objects.all():
+            writer.writerow(record.export_data)
