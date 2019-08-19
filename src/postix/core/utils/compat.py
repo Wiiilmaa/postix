9afe8896e160d5f6004dbc7d1136919d21cb064d
Tobias Kunze <r@rixx.de>
2019-08-20 01:20:26 +0200

    Fix linter

--- src/postix/core/utils/compat.py
+++ src/postix/core/utils/compat.py
@@ -5,4 +5,4 @@ try:
     detail_route = partial(action, detail=True)
     list_route = partial(action, detail=False)
 except ImportError:
-    from rest_framework.decorators import detail_route, list_route
+    from rest_framework.decorators import detail_route, list_route  # noqa
