a0e30bbd30dd47f2ddfbd1a3d175254b623c1ff3
Raphael Michel <mail@raphaelmichel.de>
2019-08-19 10:06:54 +0200

    Fix #9 -- Remove chosen items from further drop-downs

--- src/postix/static/backoffice/js/sessionform.js
+++ src/postix/static/backoffice/js/sessionform.js
@@ -1,15 +1,31 @@
 var table_body = $('#session-items_table tbody');
 
+var removeSelected = function () {
+  var selected = [];
+  table_body.find("select").each(function () {
+    selected.push($(this).val());
+  });
+  table_body.find("option:not([value=''])").each(function () {
+    var $opt = $(this);
+    $(this).prop("disabled", selected.indexOf($opt.val()) > -1 && $opt.val() !== $(this).closest("select").val());
+  });
+};
+
 var addAnotherLine = function() {
     var form_count = table_body.children().length;
+    if (form_count === $(table_body.find('select').first().find('option')).length) {
+      return;
+    }
     var item_form = $('#item-template').html().replace(/__prefix__/g, form_count);
-    $(table_body.find('> :last-child select')).off('change');
-    table_body.append(item_form)
+    $(table_body.find('> :last-child select')).off('change', addAnotherLine);
+    table_body.append(item_form);
     $('#id_items-TOTAL_FORMS').val(form_count+1);
     $(table_body.find('> :last-child select')).change(addAnotherLine);
+    $(table_body.find('> :last-child select')).change(removeSelected);
 };
 
 $(table_body.find('> :last-child select')).change(addAnotherLine);
+$(table_body.find('> :last-child select')).change(removeSelected);
 $('#add_more').click(addAnotherLine);
 
 var users = new Bloodhound({
