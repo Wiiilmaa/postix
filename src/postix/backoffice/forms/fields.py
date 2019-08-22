b50ce4cf3c46cc381ca997374d385625f351b18f
Tobias Kunze <r@rixx.de>
2019-08-23 00:32:47 +0200

    Add missing file

---
+++ src/postix/backoffice/forms/fields.py
@@ -0,0 +1,23 @@
+from django import forms
+
+
+class CalculatorWidget(forms.NumberInput):
+    def __init__(self, *args, attrs=None, **kwargs):
+        attrs = attrs or dict()
+        attrs['type'] = 'text'
+        attrs['class'] = 'calculatable'
+        super().__init__(*args, attrs=attrs, **kwargs)
+
+
+class RelaxedDecimalField(forms.DecimalField):
+    widget = CalculatorWidget
+
+    def to_python(self, value):
+        return super().to_python(
+            value.replace(",", ".") if isinstance(value, str) else value
+        )
+
+    def validate(self, value):
+        return super().validate(
+            value.replace(",", ".") if isinstance(value, str) else value
+        )
