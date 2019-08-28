0d1d30b6cc080aebc92b5d0fe495102b436b5e38
Tobias Kunze <r@rixx.de>
2019-08-28 09:23:43 +0200

    Report exporter improvements: --force

--- src/postix/backoffice/management/commands/export_reports.py
+++ src/postix/backoffice/management/commands/export_reports.py
@@ -4,22 +4,34 @@ import shutil
 from django.core.management.base import BaseCommand
 from django.utils.timezone import now
 
+from postix.backoffice.report import generate_record
 from postix.core.models import Record
 
 
 class Command(BaseCommand):
     help = 'Creates a directory called `export-yyyy-mm-dd-hhmmss` and collects copies of every session\'s most recent record.'
 
-    def handle(self, *args, **kwargs):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--force',
+            action='store_true',
+            help='Generate missing documents',
+        )
+
+    def handle(self, *args, **options):
         export_dir = os.path.join(
             '.', 'export-{}'.format(now().strftime('%Y-%m-%d-%H%M%S'))
         )
         os.mkdir(export_dir)
         successful_exports = 0
         failed_exports = []
+        force = options['force']
 
         for record in Record.objects.all():
             report_path = record.record_path
+            if not report_path and force:
+                generate_record(record)
+                report_path = record.record_path
             if report_path:
                 shutil.copy2(report_path, export_dir)
                 successful_exports += 1
@@ -32,7 +44,7 @@ class Command(BaseCommand):
         self.stdout.write(self.style.SUCCESS(success_msg))
 
         if failed_exports:
-            warn_msg = 'Could not find files for {} records (IDs: {}).'.format(
+            warn_msg = 'Could not find files for {} records (IDs: {}). Use --force to generate them.'.format(
                 len(failed_exports), failed_exports
             )
             self.stdout.write(self.style.WARNING(warn_msg))
